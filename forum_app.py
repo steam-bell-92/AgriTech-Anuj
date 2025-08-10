4e06c0bbe23b7c1e9c6b91e588df96e2804144f1
zainebp11 <zainebpatel11@gmail.com>
2025-08-10 12:56:36 +0530

    Added Farmer Forum feature

---
+++ forum_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)  # Allow frontend (Live Server) to call API
+
+
+forum_posts = []
+
+
+@app.route('/forum', methods=['GET', 'POST'])
+def forum_api():
+    if request.method == 'POST':
+        data = request.get_json()
+        forum_posts.append(data)
+        return jsonify({"status": "success"})
+    else:
+        return jsonify(forum_posts)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+
+
